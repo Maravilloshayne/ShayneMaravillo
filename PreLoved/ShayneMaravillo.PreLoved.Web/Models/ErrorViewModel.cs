a3be34c87f6c1d77a750fddc7a430b3989a1a689
Shayne Maravillo <yashnedobasa@gmail.com>
2018-08-09 19:26:49 +0800

    Setup Project

---
+++ PreLoved/ShayneMaravillo.PreLoved.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ShayneMaravillo.PreLoved.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
